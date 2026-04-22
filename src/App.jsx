b85d89f30fb4cdd055cb09f8784304a62ac8c540
Inventam <inventam@kind-brahmagupta.122-170-11-110.plesk.page>
2026-04-22 10:29:39 +0530

    edit files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects & Deploy on git static page</h2>
+      <h2>Complete course with 2 projects & Deploy on git static page!!</h2>
     </>
   )
 }
